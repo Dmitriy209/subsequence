ef1d9ba0a4e9095f0e4b99509e18f4664ba76c9d
Dmitriy209 <dim286754@gmail.com>
2024-06-16 11:48:52 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace subsequence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int startCycle = 5;
+            int endCycle = 103;
+            int stepCycle = 7;
+            string subsequence = Convert.ToString(startCycle);
+
+            for (int i = startCycle; i <= endCycle; i += stepCycle)
+            {
+                if (i != 5)
+                {
+                    subsequence = subsequence + " " + i;
+                    if (i == endCycle)
+                    {
+                        Console.WriteLine(subsequence);
+                    }
+                }
+            }
+        }
+    }
+}
