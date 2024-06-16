a9476f5c2d17b59c21b469f87908dc60a2bf95ed
Dmitriy209 <dim286754@gmail.com>
2024-06-16 20:49:11 +0500

    Ver.2

--- Program.cs
+++ Program.cs
@@ -9,18 +9,10 @@ namespace subsequence
             int startCycle = 5;
             int endCycle = 103;
             int stepCycle = 7;
-            string subsequence = Convert.ToString(startCycle); //Я забыл добавить объяснение выбора циклов
 
-            for (int i = startCycle; i <= endCycle; i += stepCycle) //С помощью цикла for можно дойти от 5 до 103 за наименьшее количество итерация
+            for (int i = startCycle; i <= endCycle; i += stepCycle)
             {
-                if (i != 5) //Цикл if нужен для того, чтобы убрать лишнюю 5 в начале цикла, я думаю можно обойтись без него, но не знаю как
-                {
-                    subsequence = subsequence + " " + i;
-                    if (i == endCycle) //Цикл if считаю достаточным для проверки окончания программы, хотя можно заменить на while или switch
-                    {
-                        Console.WriteLine(subsequence);
-                    }
-                }
+                Console.WriteLine(i);
             }
         }
     }
